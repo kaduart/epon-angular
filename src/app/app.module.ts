727c2199fa6ff285d60bf0cd6262850d62859316
Matheus Freitas <matheus.freitas@ibti.net.br>
2019-12-12 12:13:29 -0300

    resolução de conflitos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import 'materialize-css';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './security/sign-up/sign-up.component';
 import { SignInComponent } from './security/sign-in/sign-in.component';
@@ -15,16 +13,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { httpInterceptProviders } from './http-interceptor';
 import { CookieService } from 'ngx-cookie-service';
 import {ProgressBarModule} from 'angular-progress-bar';
-<<<<<<< HEAD
 import { PersonalComponent } from './user/sign-up/personal/personal.component';
 import { ComplementComponent } from './user/sign-up/complement/complement.component';
 import { NavbarComponent } from './user/sign-up/navbar/navbar.component';
 import { FormDataService } from './user/sign-up/data/form-data.service';
 import { PasswordComponent } from './user/sign-up/password/password.component';
 import { FinishComponent } from './user/sign-up/finish/finish.component';
-=======
 import { HeaderComponent } from './header/header.component';
->>>>>>> SSIDG-1657_Portal_de_login
+
 
 @NgModule({
   declarations: [
